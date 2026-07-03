35cf1119b0168f50353766fff8d01e45309c9952
Giorgi <khabazishvili20@gmail.com>
2026-07-03 12:03:37 -0700

    Added status codes

--- src/api/core/base_models.py
+++ src/api/core/base_models.py
@@ -7,6 +7,7 @@ from typing import Any
 class BaseConfig:
     api_base_url: str
 
+
 @dataclass
 class RequestData:
     uri: str
@@ -14,10 +15,20 @@ class RequestData:
     json: dict[str, Any] | None
     headers: dict
 
+
 class DataType(Enum):
     JSON = "json"
     FORM_DATA = "form_data"
     JSON_WITH_NULL = "json_with_null"
 
+
 class HttpStatus(Enum):
     OK = 200
+    CREATED = 201
+    BAD_REQUEST = 400
+    UNAUTHORIZED = 401
+    FORBIDDEN = 403
+    NOT_FOUND = 404
+    METHOD_NOT_ALLOWED = 405
+    INTERNAL_SERVER_ERROR = 500
+    NOT_IMPLEMENTED = 501
--- tests/test_automation_site.py
+++ tests/test_automation_site.py
@@ -5,5 +5,7 @@ import pytest
 def test_get_automation_site_room(automation_service):
     res = automation_service.get_booking_room()
 
-    assert res.room_id
-    assert res.accessible
+    assert (
+        res.description
+        == "Aenean porttitor mauris sit amet lacinia molestie. In posuere accumsan aliquet. Maecenas sit amet nisl massa. Interdum et malesuada fames ac ante."
+    )
