ab3298e8e48288e3a75b4c225b456d87b075a919
Giorgi <khabazishvili20@gmail.com>
2026-07-11 15:31:31 -0700

    Renamed url param

--- src/fixtures/page.py
+++ src/fixtures/page.py
@@ -6,7 +6,7 @@ from src.web.core.base_page import BasePage
 @pytest.fixture
 def page_factory(page, base_config):
     def _wrapper(pageobj: type[BasePage]):
-        _page = pageobj(page=page, url=base_config.api_base_url)
+        _page = pageobj(page=page, url=base_config.base_url)
         _page.open()
         return _page
 
